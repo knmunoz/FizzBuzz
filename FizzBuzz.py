80174bcd8eca4496697ba3a7e386a2a0f973e5e9
Karina Munoz <munozka@oregonstate.edu>
2021-03-04 14:30:56 -0800

    code passes second test

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,10 @@
 def multiples(num):
-    if num % 3 == 0:
+    if (num % 3 == 0) and (num % 5 == 0):
+        return "FizzBuzz"
+    elif num % 3 == 0:
         return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
     else:
         return 1
     

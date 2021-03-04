b879202ed595c70188f2b73bd132a2152ab99ef3
Karina Munoz <munozka@oregonstate.edu>
2021-03-04 14:27:12 -0800

    code passes first test

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,9 @@
 def multiples(num):
-    pass
+    if num % 3 == 0:
+        return "Fizz"
+    else:
+        return 1
     
 for i in range (1, 101):
     result = multiples(i)
 
-    if result == 1:
-        print(i)
--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -3,7 +3,7 @@ import FizzBuzz
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(FizzBuzz, " ")
+        self.assertEqual(FizzBuzz.multiples(3), "Fizz")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)

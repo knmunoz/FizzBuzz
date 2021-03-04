fce927d1cff954a22bd129c347f722cae631e41b
Karina Munoz <munozka@oregonstate.edu>
2021-03-04 14:30:17 -0800

    second test, assertEqual

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -4,6 +4,8 @@ import FizzBuzz
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(FizzBuzz.multiples(3), "Fizz")
+    def test2(self):
+        self.assertEqual(FizzBuzz.multiples(15), "FizzBuzz")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
